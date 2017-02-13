18645807fdf46aed91b1ebb7b5893ea304ad862a
Mary <mary.land28120@gmail.com>
2017-02-13 15:16:47 +0000

    my comment

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //Write your function calls below this line
+function setColor(row, col, color) {
+            var cell = $("#t")[0].rows[row].cells[col];
+            cell.style.backgroundColor = color;
+             }
+             
+             setColor(3, 5, "red");
+             setColor(3, 6, "red");
+             setColor(4, 5, "red");
+             setColor(4, 6, "red");
+             setColor(3, 2, "green");
+             setColor(4, 2, "green");
+             setColor(3, 1, "green");
+              setColor(4, 1, "green");
+             
+             
+             
+             
+             
+             
+        
